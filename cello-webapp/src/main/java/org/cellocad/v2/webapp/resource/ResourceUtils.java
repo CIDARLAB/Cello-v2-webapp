3ddc81c49ca2057aba133e26119bbf1fed727c33
Timothy Jones <jonests@bu.edu>
2020-05-12 11:51:52 -0400

    Remove calls to deprecated methods.

--- cello-webapp/src/main/java/org/cellocad/v2/webapp/resource/ResourceUtils.java
+++ cello-webapp/src/main/java/org/cellocad/v2/webapp/resource/ResourceUtils.java
@@ -1,22 +1,20 @@
 /*
- * Copyright (C) 2020 Boston University (BU) Permission is hereby granted, free
- * of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software,
- * and to permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
+ * Copyright (C) 2020 Boston University (BU)
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 package org.cellocad.v2.webapp.resource;
@@ -139,7 +137,7 @@ public class ResourceUtils {
   }
 
   private static void initMetaDataFile(final String filepath) throws IOException {
-    Utils.createFileIfNonExistant(filepath);
+    Utils.createFile(filepath);
     // write empty array
     final ObjectMapper mapper = new ObjectMapper();
     final ArrayNode arr = mapper.createArrayNode();
@@ -167,7 +165,7 @@ public class ResourceUtils {
 
   private static void initTargetDataResources(
       final String dir, final String filepath, final String pattern) throws IOException {
-    Utils.createDirectoryIfNonExistant(dir);
+    Utils.makeDirectory(dir);
     ResourceUtils.initMetaDataFile(filepath);
     final PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
     final Resource[] resources = resolver.getResources(pattern);
@@ -190,7 +188,7 @@ public class ResourceUtils {
   }
 
   private static void initTargetDataResources() throws IOException {
-    Utils.createDirectoryIfNonExistant(ResourceUtils.getTargetDataResourcesDirectory());
+    Utils.makeDirectory(ResourceUtils.getTargetDataResourcesDirectory());
     String dir = "";
     String metadata = "";
     // user constraints
@@ -265,7 +263,7 @@ public class ResourceUtils {
   }
 
   private static void initSettingsResources() throws IOException {
-    Utils.createDirectoryIfNonExistant(ResourceUtils.getSettingsResourcesDirectory());
+    Utils.makeDirectory(ResourceUtils.getSettingsResourcesDirectory());
     ResourceUtils.initSettingsFile();
   }
 
@@ -275,7 +273,7 @@ public class ResourceUtils {
    * @throws IOException Unable to read or write resources.
    */
   public static void initResources() throws IOException {
-    Utils.createDirectoryIfNonExistant(ApplicationUtils.getResourcesDirectory());
+    Utils.makeDirectory(ApplicationUtils.getResourcesDirectory());
     ResourceUtils.initTargetDataResources();
     ResourceUtils.initSettingsResources();
   }
